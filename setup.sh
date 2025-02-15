94a95dd946974b9bf70e77d5010c79b0dbe5cb83
dtanigaw <dtanigaw@student.42.fr>
2025-02-15 02:38:15 +0100

    SSH works + Add .env example

--- setup.sh
+++ setup.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
-# Check if we should use a persistent onion address or not
-# if [ "$ONION_PERSISTENT" == "true" ]; then
-#     echo "Setting up persistent onion address..."
-#     # Create a persistent hidden service
-#     mkdir -p /var/lib/tor/hidden_service
-#     chown -R debian-tor:debian-tor /var/lib/tor/hidden_service
-#     chmod -R 700 /var/lib/tor/hidden_service
-
-#     # Configure Tor for a persistent onion address
-#     echo "HiddenServiceDir /var/lib/tor/hidden_service/" >> /etc/tor/torrc
-# else
-#     echo "Setting up non-persistent onion address..."
-    # Configure Tor for a non-persistent hidden service (no specific directory)
-# fi
-
-# echo "HiddenServicePort 80 127.0.0.1:80" >> /etc/tor/torrc
-# echo "HiddenServicePort 4242 127.0.0.1:22" >> /etc/tor/torrc
+# Tor
 
 # Fix permissions for the hidden service directory
 # Permissions are overridden if done from the Dockerfile
 chown -R debian-tor:debian-tor /var/lib/tor
 chmod -R 700 /var/lib/tor
 
-# Start the services
+
+# SSH
+
+# Set default values if not provided
+# (`:-` is the default value operator in bash)
+SSH_USER=${SSH_USER:-admin}
+SSH_PASS=${SSH_PASS:-password}
+
+# Create the user (if not exists) and set password
+useradd -m -s /bin/bash "$SSH_USER"
+echo "$SSH_USER:$SSH_PASS" | chpasswd
+
+
+# Services
 
 # Start Tor
 service tor start
-
 # Start SSH
 service ssh start
-
 # Start Nginx in the foreground (to keep the container running)
 nginx -g 'daemon off;'
